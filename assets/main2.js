7448321ffe2a230ea456018b9c800477a7d7b8e9
Admin Jaime <admin.jaime@aall.local>
2019-12-18 12:48:50 -0700

    added Endo section

--- assets/main2.js
+++ assets/main2.js
@@ -14,7 +14,9 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 let csrNames = db.collection("Survivors");
+let endoNames = db.collection("Endos");
 let mainObject = [];
+let endoObject = [];
 
 //getting date of last Friday, formatting to match the string in Firebase records
 const t = new Date().getDate() + (6 - new Date().getDay() -1) -7;
@@ -29,6 +31,7 @@ if (new Date().getDay() == 5) {
 
 console.log(lastWeek);
 let tempObject = new Object();
+let tempEndoObject = new Object();
 // let query = csrNames.where('dateCreated', '==', `${lastWeek}`)
 //     .get()
 //     .then(snapshot => {
@@ -51,7 +54,7 @@ function getNames() {
     csrNames.get()
     .then(snapshot => {
         snapshot.forEach(doc => {
-            console.log(doc.data().CSR);
+            // console.log(doc.data().CSR);
             if(doc.data().dateCreated == `${lastWeek}`) {
                 tempObject['Name'] = doc.data().Name;
                 tempObject['Tribe'] = doc.data().Tribe;
@@ -71,6 +74,26 @@ function getNames() {
     })
 }
 
+function getEndoNames() {
+    endoNames.get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            //${lastWeek}
+            console.log('end doc.data', doc.data().dateCreated);
+            if(doc.data().dateCreated == `20191218`) {
+                tempEndoObject['Name'] = doc.data().Name;
+                tempEndoObject['Tribe'] = doc.data().Tribe;
+                tempEndoObject['Streak'] = doc.data().Streak;
+                mainObject.push(tempEndoObject);
+                addSingleEndoName(tempEndoObject)
+            }
+        })
+    })
+    .catch(err => {
+        console.log(`Error getting documents. ${err}`)
+    })
+}
+
 // function addTopNames() {
 //     for (i=0; i<=2; i++) {
 //         const newTopName = "<h2 class='topName'>" + namesArray[i] + "</h2>";
@@ -78,6 +101,16 @@ function getNames() {
 //         $('#nameField').append(newTopName);
 //     }
 // } 
+
+function addSingleEndoName(obj) {
+    let newEndoName = "";
+    let emoji7 = "<span aria-role='image' aria-label='thumbs up'>👍</span>";
+    newEndoName = "<h3 class='lowName'>" + obj["Name"] + " <span class='tribe'>(" + 
+    obj["Tribe"] + ")</span> <span class='streak'>" + obj["Streak"] + " days<span> " + emoji7 + "</h3>";
+    $('#endNameField').append("<div class='row'></div>");
+    $('#endNameField').append(newEndoName);
+
+}
 
 function addSingleName(obj) {
     //console.log(obj);
@@ -168,4 +201,5 @@ function topNameAnimation() {
 
 $(document).ready(function () {
     getNames();
+    getEndoNames();
 })
